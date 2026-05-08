f130cd29598549cca378f388a9be7b7a1e3a2966
Tirtha Samanta <samantatirtha904@gmail.com>
2026-05-08 15:47:50 +0530

    Refactor auth routes for improved structure

--- routes/auth.js
+++ routes/auth.js
@@ -120,8 +120,8 @@ router.post("/login", async (req, res) => {
     // ✅ Cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // 🔥 true in production (HTTPS)
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       path:"/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
