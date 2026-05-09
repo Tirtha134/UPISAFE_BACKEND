22eaed93cc01c38191b6d8a9637ff1c0b6aa4b01
Tirtha Samanta <samantatirtha904@gmail.com>
2026-05-09 12:16:06 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: [true, "Phone is required"],
+      unique: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true, // adds createdAt & updatedAt automatically
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
